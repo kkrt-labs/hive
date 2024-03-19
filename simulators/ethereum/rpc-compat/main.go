070b365b5d59f8ae0aea4d93e12199c7be9dabc9
Gregory Edison <gregory.edison1993@gmail.com>
2024-03-19 15:22:31 +0100

    skip withdrawalsRoot

--- simulators/ethereum/rpc-compat/main.go
+++ simulators/ethereum/rpc-compat/main.go
@@ -191,7 +191,20 @@ func cleanKakarot(resp, expectedData string) (string, string) {
 // cleanBlockData modifies the response and expected data if the returned values match a block information.
 func cleanBlockData(resp, expectedData string) (string, string) {
 	// TODO: remove the miner, gasLimit and baseFeePerGas skip once we have a way to set these in Kakarot.
-	var fields = []string{"result.hash", "result.parentHash", "result.timestamp", "result.baseFeePerGas", "result.difficulty", "result.gasLimit", "result.miner", "result.size", "result.stateRoot", "result.totalDifficulty", "result.withdrawals"}
+	var fields = []string{
+		"result.hash",
+		"result.parentHash",
+		"result.timestamp",
+		"result.baseFeePerGas",
+		"result.difficulty",
+		"result.gasLimit",
+		"result.miner",
+		"result.size",
+		"result.stateRoot",
+		"result.totalDifficulty",
+		"result.withdrawals",
+		"result.withdrawalsRoot",
+	}
 	resp = deleteFields(resp, fields...)
 	expectedData = deleteFields(expectedData, fields...)
 
